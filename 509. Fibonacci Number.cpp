3fafe2a85b6a3c6cad25f084f729fb8ca57f96e0
askdhruv <92171175+askdhruv@users.noreply.github.com>
2023-04-28 16:40:36 +0530

    Update 509. Fibonacci Number.cpp

--- 509. Fibonacci Number.cpp
+++ 509. Fibonacci Number.cpp
@@ -3,18 +3,17 @@ public:
     int fib(int n) {
         if (n==0)
             return 0;
-        else if(n<2){
-            return 1;
-        }
+        else if(n<2)
+            return 1;        
         int arr[n];
-        arr[0]=0;
-        arr[1]=1;
-       for(int i = 1; i<n-1; i++){
-           arr[i+1] = arr[i] + arr[i-1];
+            arr[0]=0;
+            arr[1]=1;
+        for(int i = 1; i<n-1; i++){
+            arr[i+1] = arr[i] + arr[i-1];
        }
-        int sum = 0;
+            int sum = 0;
 
-        sum = arr[n-1] + arr[n-2];
+            sum = arr[n-1] + arr[n-2];
     return sum;
     }
 };
